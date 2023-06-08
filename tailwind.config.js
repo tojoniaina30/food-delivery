0fffb1b98922746613d6a33fd1eca9213cd5a6e4
tojoniaina <tojorar@gmail.com>
2023-06-08 11:32:46 +0300

    config theme fontsize

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,21 @@ export default {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    fontSize: {
+      '2xs': '10px',
+      'xs': '11px',
+      'sm': '12px',
+      'tiny': '13px',
+      'base': '14px',
+      'lg': '18px',
+      'xl': '20px',
+      '2xl': '32px',
+      '3xl': '40px',
+      '4xl': '60px',
+    }
   },
   plugins: [
-    
+
   ],
 }
 
